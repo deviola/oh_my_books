d4e10189edfd59999515f6329bab112100816f8e
Wiola <w.ledwozyw@gmail.com>
2022-12-12 22:24:27 +0100

    swagger config

--- src/main/java/com/app/ohmybooks/OhmybooksApplication.java
+++ src/main/java/com/app/ohmybooks/OhmybooksApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableConfigurationProperties({AppProperties.class})
 @EnableJpaRepositories
+@EnableSwagger2
 public class OhmybooksApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/app/ohmybooks/config/SwaggerConfig.java
+++ src/main/java/com/app/ohmybooks/config/SwaggerConfig.java
@@ -1,2 +1,39 @@
-package com.app.ohmybooks.config;public class SwaggerConfig {
+package com.app.ohmybooks.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(createApiInfo());
+    }
+
+    private ApiInfo createApiInfo() {
+        return new ApiInfo("Library API",
+                "Library database",
+                "1.00",
+                "https://github.com/deviola",
+                new Contact("deViola", "https://github.com/deviola", "wk.ostasz@gmail.com"),
+                "test licence",
+                "https://github.com/deviola",
+                Collections.emptyList());
+    }
+
 }
